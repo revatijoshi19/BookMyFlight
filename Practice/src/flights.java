9c6389fe2bdebd6b504db1a7582a0bc7bdcd5415
Revati Joshi <rjoshi@rjoshi-inl.local>
2017-03-04 00:49:43 +0530

    cleaning

--- Practice/src/flights.java
+++ Practice/src/flights.java
@@ -15,36 +15,6 @@ public class flights {
         WebDriver driver = new ChromeDriver();
         try {
 
-
-//            String baseUrl = "https://www.makemytrip.com/";
-//            driver.get(baseUrl);
-//
-//            driver.findElement(By.cssSelector("#ch_login_icon")).click();
-//
-//            WebElement myElement = driver.findElement(By.cssSelector("#ch_login_email"));
-//            myElement.sendKeys("revatijoshi19@gmail.com");
-//
-//            WebElement myElement1 = driver.findElement(By.cssSelector("#ch_login_password"));
-//            myElement1.sendKeys("password123");
-//
-//            driver.findElement(By.cssSelector("#header_tab_flights")).click();
-//
-//            driver.findElement(By.cssSelector("#js-switch__option #switch__input_2")).click();
-//
-//            driver.findElement(By.cssSelector("#hp-widget__sfrom")).click();
-//            WebElement element = driver.findElement(By.cssSelector("#hp-widget__sfrom"));
-//            Select se = new Select(element);
-//            se.selectByValue("Goa, India");
-//
-//            WebElement element1 = driver.findElement(By.cssSelector("#hp-widget__sTo"));
-//            Select se1 = new Select(element);
-//            se1.selectByValue("Pune, India");
-//
-//            WebElement element2 = driver.findElement(By.cssSelector("#hp-widget__depart"));
-//            Select se2 = new Select(element);
-//
-//            driver.findElement(By.cssSelector("#hp-widget__return")).click();
-
             String baseUrl = "https://www.goibibo.com";
             driver.get(baseUrl);
 
@@ -53,9 +23,6 @@ public class flights {
 
             driver.findElement(By.cssSelector("#jrdp_end-calen_3_21_2017")).click();
             Thread.sleep(10000);
-
-//            driver.findElement(By.cssSelector("#start-date")).click();
-//            Thread.sleep(30000);
 
             WebElement myElement = driver.findElement(By.cssSelector("#gi_source_st"));
             myElement.sendKeys("Mumbai");
@@ -71,9 +38,6 @@ public class flights {
 
             driver.findElement(By.cssSelector("#jrdp_start-calen_3_1_2017")).click();
             Thread.sleep(10000);
-
-//            driver.findElement(By.cssSelector("#jrdp_end-calen_4_21_2017")).click();
-//            Thread.sleep(10000);
 
             driver.findElement(By.cssSelector("#searchWidgetNew")).click();
             Thread.sleep(10000);
@@ -117,29 +81,7 @@ public class flights {
 
             driver.findElement(By.cssSelector("#makePayment")).click();
             Thread.sleep(10000);
-
-//            driver.findElement(By.cssSelector("#RoundTrip")).click();
-//
-//            WebElement myElement = driver.findElement(By.cssSelector("#FromTag"));
-//            myElement.sendKeys("Mumbai");
-//
-//            WebElement myElement1 = driver.findElement(By.cssSelector("#ToTag"));
-//            myElement1.sendKeys("oaG");
-//
-//            WebElement myElement3 = driver.findElement(By.cssSelector("#DepartDate"));
-//            myElement3.sendKeys("01/05/2017");
-//
-//            WebElement myElement4 = driver.findElement(By.cssSelector("#ReturnDate"));
-//            myElement4.sendKeys("15/05/2017");
-//
-//            driver.findElement(By.cssSelector("#Adults")).click();
-//
-//            driver.findElement(By.cssSelector(".booking")).click();
-//            Thread.sleep(10000);
-//
-//
-//            driver.findElement(By.cssSelector(".fRight")).click();
-
+            
 
         } catch (InterruptedException e) {
             e.printStackTrace();
